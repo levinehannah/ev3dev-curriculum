0a536a836f32cfb2366ece0b4c41f2e300881de8
levineha <h.levine3789@gmail.com>
2018-08-01 20:30:42 -0400

    g

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -11,13 +11,75 @@
   could be called.  That way it's a generic action that could be used in any task.
 """
 
+
 import ev3dev.ev3 as ev3
-import math
 import time
+import math
+MAX_SPEED = 900
 
 
 class Snatch3r(object):
-    """Commands for the Snatch3r robot that might be useful in many different programs."""
-    
-    # TODO: Implement the Snatch3r class as needed when working the sandox exercises
-    # (and delete these comments)
+    """Commands for the Snatch3r robot that might be useful in many
+    different programs."""
+
+    def __init__(self):
+        """ construct and store a left motor and a right motor."""
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        self.arm_motor = ev3.MediumMotor(ev3.OUTPUT_A)
+        self.touch_sensor = ev3.TouchSensor()
+        assert self.arm_motor.connected
+        assert self.touch_sensor.connected
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
+    def drive_inches(self, inches_target, speed_deg_per_second):
+        """ moves the robot by given speed for a given distance"""
+        self.left_motor.run_to_rel_pos(position_sp=inches_target * 90,
+                                       speed_sp=speed_deg_per_second,
+                                       stop_action='brake')
+        self.right_motor.run_to_rel_pos(position_sp=inches_target * 90,
+                                        speed_sp=speed_deg_per_second,
+                                        stop_action='brake')
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
+    def turn_degrees(self, degrees_to_turn, turn_speed_sp):
+        """ turns the robot"""
+        self.left_motor.run_to_rel_pos(position_sp= -degrees_to_turn * 4.45,
+                                       speed_sp= turn_speed_sp,
+                                       stop_action='brake')
+        self.right_motor.run_to_rel_pos(position_sp= degrees_to_turn * 4.45,
+                                        speed_sp=turn_speed_sp,
+                                        stop_action='brake')
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
+    def arm_calibration(self):
+        self.arm_motor.run_forever(speed_sp=MAX_SPEED)
+        while not self.touch_sensor:
+            time.sleep(0.01)
+        self.arm_motor.stop(stop_action="brake")
+        ev3.Sound.beep()
+
+        arm_revolutions_for_full_range = 14.2
+        self.arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
+        self.arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        ev3.Sound.beep()
+        self.arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
+
+    def arm_up(self):
+        self.arm_motor.run_forever(speed_sp=MAX_SPEED)
+        while not self.touch_sensor.is_pressed:
+            time.sleep(0.01)
+        self.arm_motor.stop(stop_action="brake")
+        ev3.Sound.beep()
+
+    def arm_down(self):
+        self.arm_motor.run_to_abs_pos(speed_sp=MAX_SPEED)
+        self.arm_motor.wait_while(ev3.Motor.STATE_HOLDING)
+        ev3.Sound.beep()
+
+    def shutdown(self):
+        self.arm_motor.stop(stop_action='brake')
+        self.left_motor.stop(stop_action='brake')
+        self.right_motor.stop(stop_action='brake')
+        ev3.Leds.all_off()
